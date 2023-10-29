80c4628b82359e314c06ac56b42f33070880a49f
Wanderson_RB <wandersonrb.dev@gmail.com>
2023-10-29 19:03:16 -0300

    Create divisionfunction

--- script.js
+++ script.js
@@ -13,4 +13,9 @@ function multiplication(factor1, factor2) {
     return factor1 * factor2;
 }
 
+function division(num1, num2) {
+    if (num2 === 0) return null;
+    return num1 / num2;
+}
+
 main()
